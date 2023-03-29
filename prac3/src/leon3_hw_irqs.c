27be46a0ee7634cdb20cfec19be001d49d067d1a
dasiUAH <75973341+dasiUAH@users.noreply.github.com>
2023-03-29 09:51:19 +0200

    Update leon3_hw_irqs.c

--- prac3/src/leon3_hw_irqs.c
+++ prac3/src/leon3_hw_irqs.c
@@ -19,7 +19,8 @@ uint8_t leon3_clear_irq (uint8_t irq_level) {
 	uint8_t error=0;
 	if((irq_level >0) && (irq_level <16)){
 		uint32_t bit = (1 << irq_level); // Mask irq level to clear
-		*LEON3_ICLEAR = bit;	//Sets that bit to 1
+//		*LEON3_ICLEAR = bit;	//Sets that bit to 1
+		*LEON3_ICLEAR |= bit;	//Sets that bit to 1
 	}else
 		error=1;
 	return error;
@@ -33,7 +34,8 @@ uint8_t leon3_force_irq (uint8_t irq_level) {
 	if((irq_level >0) && (irq_level <16)){
 		// COMPLETADhecho. Poniendo a 1 SOLO el bit de LEON3_IFORCE correspondiente al irq_level
 		uint32_t bit = (1 << irq_level);
-		*LEON3_IFORCE = bit;
+//		*LEON3_IFORCE = bit;
+		*LEON3_IFORCE |= bit;
 	}else
 		error=1;
 	return error;
@@ -48,9 +50,10 @@ uint8_t leon3_unmask_irq (uint8_t irq_level){
 		// clear any pending irq of that level
 		leon3_clear_irq(irq_level);
 		// COMPLETADhecho Poniendo a 1 SOLO el bit de LEON3_IMASK correspondiente al irq_level
-		*LEON3_IMASK = 0;
+//		*LEON3_IMASK = 0;  // LO BORRAS TODO
 		uint32_t bit = (1 << irq_level);
-		*LEON3_IMASK = bit;
+//		*LEON3_IMASK = bit;
+		*LEON3_IMASK |= bit;
 	}else
 		error=1;
 	return error;
@@ -63,10 +66,11 @@ uint8_t leon3_mask_irq (uint8_t irq_level) {
 	uint8_t error=0;
 	if(irq_level >0 && irq_level <16){
 		// COMPLETADhecho  Poniendo a 0 SOLO el bit de LEON3_IMASK correspondiente al irq_level
-		*LEON3_IMASK  = 0xFFFE;
+//		*LEON3_IMASK  = 0xFFFE;  // LO CAMBIAS TODO
 		uint32_t bit = (1 << irq_level);
-		bit = bit ^ 0xffff;
-		*LEON3_IMASK = bit;
+		bit = bit ^ 0xffff;	// MEJOR bit = ~bit;
+//		*LEON3_IMASK = bit;
+		*LEON3_IMASK &= bit;
 	}else
 		error=1;
 	return error;
